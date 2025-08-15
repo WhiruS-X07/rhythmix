9cd38522f5d31628e9dc7204325c4b3ecaabfa13
Piyush Kumar <piyush072003+github@gmail.com>
2025-08-15 16:10:36 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -19,14 +19,14 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-    $mail->Username = 'piyush1672003@gmail.com';                     //SMTP username
-    $mail->Password = 'dbrnkwlvbvpvpnkr';                               //SMTP password
+    $mail->Username = 'example@gmail.com';                     //SMTP username
+    $mail->Password = '';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
     $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('piyush1672003@gmail.com', 'Trial');
-    $mail->addAddress('piyush072003@gmail.com', 'Trial User');     //Add a recipient
+    $mail->setFrom('example@gmail.com', 'Trial');
+    $mail->addAddress('example@gmail.com', 'Trial User');     //Add a recipient
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
